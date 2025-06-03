50cf950b6d9df572479492f23c709bd40e72d332
Juha Vierinen <jvierine@gmail.com>
2025-06-03 13:53:29 +0000

    fixed reference frame

--- asteroid_uncertainty.py
+++ asteroid_uncertainty.py
@@ -11,62 +11,68 @@ spice.furnsh("pck00010.tpc")     # Planetary constants kernel
 spice.furnsh("earth_latest_high_prec.bpc")
 
 
-epoch_et0 = spice.str2et("2025-01-01T00:00:00")
+epoch_et0 = spice.str2et("2025-05-05T00:00:00")
 
 def observe_asteroid(kep=n.array([2*sc.au/1e3,0.3,10,80,45,30]),
-                     epoch_et=epoch_et0+3600):
-    
-                
-      #                       300000000,     # Semi-major axis in km (e.g., 2 AU ≈ 300 million km)
-     #   0.1,           # Eccentricity
+                     epoch_et=epoch_et0+3600):    
+    #                       300000000,     # Semi-major axis in km (e.g., 2 AU ≈ 300 million km)
+    #   0.1,           # Eccentricity
     #    np.radians(10),     # Inclination
-        #np.radians(80),     # RAAN
-       # np.radians(45),     # Argument of periapsis
-      #  np.radians(30),      # Mean anomaly
-     #   epoch_et,
+    #np.radians(80),     # RAAN
+    # np.radians(45),     # Argument of periapsis
+    #  np.radians(30),      # Mean anomaly
+    #   epoch_et,
     #    mu_sun
-
+    
     # 2. Observer info (Geodetic coordinates)
     # Example: lat/lon in degrees, height in km
     lat = 69.6496      # Tromsø
     lon = 18.9560
     alt = 0.01         # 10 meters above sea level
-
+    
     # Convert to radians
     lat_rad = np.radians(lat)
     lon_rad = np.radians(lon)
-
-
+    
     # Convert to radians
     lat_rad = np.radians(lat)
     lon_rad = np.radians(lon)
 
     # Time of observation
-    utc = '2025-06-03T12:00:00'
-    et = spice.str2et(utc)
+#    utc = '2025-05-05T00:00:00'
+ #   et = spice.str2et(utc)
 
     earth_re=spice.bodvrd("EARTH", "RADII", 3)[1][0]
 #    print(earth_re)
     earth_flattening = 1 / 298.257223563
  #   print(earth_flattening)
 
-
     # Compute position of the observer relative to Earth center
     # Output in ITRF93 frame, can convert later
     pos = spice.georec(lon_rad, lat_rad, alt, earth_re, earth_flattening)
-  #  print(pos)
+    #  print(pos)
+    xform = spice.sxform("ITRF93", "J2000", epoch_et)
+    xform=np.ascontiguousarray(xform[:3,:3])
+    obs_j2000 = spice.mxvg(xform, pos)
 
     # Convert to IAU_EARTH frame
     obs_pos = spice.recgeo(pos, earth_re, earth_flattening)
-   # print(obs_pos)
-
-
+    # print(obs_pos)
+    earth_pos, _ = spice.spkpos("EARTH", epoch_et, "J2000", "NONE", "SUN")
+    obs_helio_j2000 = [earth_pos[i] + obs_j2000[i] for i in range(3)]
+    
     # Transform observer position to J2000 frame
-    state = spice.sxform("ITRF93", "J2000", et)
-   # print(state)
-    matrix = np.ascontiguousarray(state[:3, :3])
-    vector = np.ascontiguousarray(pos)
-    observer_j2000 = spice.mxv(matrix,vector)
+    state = spice.sxform("ITRF93", "J2000", epoch_et)
+
+    # Convert to heliocentric ecliptic (IAU76/J2000)
+    rotation = spice.pxform("J2000", "ECLIPJ2000", epoch_et)
+    rotation=np.ascontiguousarray(rotation)
+    obs_helio_j2000=np.ascontiguousarray(obs_helio_j2000)
+    obs_helio_eclip = spice.mxv(rotation, obs_helio_j2000)
+    # print(state)
+    #matrix = np.ascontiguousarray(state[:3, :3])
+    #vector = np.ascontiguousarray(pos)
+    #observer_j2000 = spice.mxv(matrix,vector)
 
 #    print("Observer position in J2000 frame (km):", observer_j2000)
 
@@ -80,7 +86,7 @@ def observe_asteroid(kep=n.array([2*sc.au/1e3,0.3,10,80,45,30]),
 
  #   print(epoch_et)
     kep_elements = [
-        kep[0],     # Semi-major axis in km (e.g., 2 AU ≈ 300 million km)
+        kep[0]*(1-kep[1]),     # PERIFOCAL DISTANCE = (a*(1-e)))#Semi-major axis in km (e.g., 2 AU ≈ 300 million km)
         kep[1],           # Eccentricity
         np.radians(kep[2]),     # Inclination (rad)
         np.radians(kep[3]),     # RAAN (rad)
@@ -90,9 +96,14 @@ def observe_asteroid(kep=n.array([2*sc.au/1e3,0.3,10,80,45,30]),
         mu_sun
     ]
 
+
+    # IAU76/J2000 heliocentric ecliptic osculating elements
+    #elements = [a_km, ecc, inc, raan, argp, m0, epoch_et, mu_sun]
+    
     # epoch of observations (J2000 seconds)
+    
     state_asteroid = spice.conics(kep_elements, epoch_et)
-    return(observer_j2000,state_asteroid)
+    return(obs_helio_eclip,state_asteroid)
 
 #    print("Asteroid state in J2000 (km, km/s):", state_asteroid)
 
@@ -155,15 +166,27 @@ def estimate_keplerian(obs_range,
     #xhat=sio.fmin(ss,xhat)
     print(ss(xhat))
     return(xhat)
-    
-times=epoch_et0+n.arange(10)*7*24*3600
-kep_true=n.array([2*sc.au/1e3,0.3,20,80,45,30])
-kep_guess=n.array([2.2*sc.au/1e3,0.32,22,82,47,32])
+
+# (2025 LB)
+times=epoch_et0+n.arange(30)*24*3600
+# 
+kep_true=n.array([2.158504572325312*sc.au/1e3,
+                  0.5573199074979915, # e
+                  7.977400997581499,  # inc
+                  73.8364986266037,   # node 
+                  212.6277929093889,  # aop
+                  342.498256445095])  # mean anom at epoch
+kep_guess=kep_true+n.array([0.01*sc.au/1e3,0.01,0.1,0.1,0.1,0.1])
+#kep_guess=n.array([2.01*sc.au/1e3,0.3005,20.2,80.1,45.1,30.1])
 
 obs_range,obs_dec,obs_az,ap=simulate_measurement(kep_true,times=times)
+plt.plot(times,obs_range*1e3/sc.au,".")
+plt.xlabel("Time (J2000 sec)")
+plt.ylabel("Range (AU)")
+plt.show()
 
 angle_std=5e-6
-range_std=0.1                  
+range_std=1                  
 range_err=n.random.randn(len(obs_range))*range_std
 dec_err=n.random.randn(len(obs_range))*angle_std
 az_err=n.random.randn(len(obs_range))*angle_std
@@ -172,16 +195,16 @@ xhat=estimate_keplerian(obs_range+range_err,obs_dec+dec_err,obs_az+az_err,k_gues
 xhat[1]=10**xhat[1]
 print("Estimated Keplerian parameters using angles only (RP,e,i,Omega,omega,nu)")
 print(xhat)
-print("Error (percent)")
-print( 100*(kep_true-xhat)/kep_true)
+#print("Error (percent)")
+#print( 100*(kep_true-xhat)/kep_true)
 
 xhat=estimate_keplerian(obs_range+range_err,obs_dec+dec_err,obs_az+az_err,k_guess=kep_guess,times=times,use_range=True)
 xhat[1]=10**xhat[1]
 
 print("Estimated Keplerian parameters using angles and range (RP,e,i,Omega,omega,nu)")
 print(xhat)
-print("Error (percent)")
-print( 100*(kep_true-xhat)/kep_true)
+#print("Error (percent)")
+#print( 100*(kep_true-xhat)/kep_true)
 
 
 #print(xhat)
